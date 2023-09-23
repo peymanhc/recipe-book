d03d2918127fbb5d22c3eeaffe657ed5bc51b9eb
p.hadavi <peymanhc@gmail.com>
2023-09-24 00:17:48 +0330

    :up: (recipe Detail): add recipe Detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A title',
+      'A description',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_LDgQPhGxwDZ8jJ9bbRH-SUUmPYS8RXqssm8v5vKtQi8DrR6bhihftoIl4ZQsKQZ4XKE&usqp=CAU'
+    ),
+    new Recipe(
+      'A title',
+      'A description',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_LDgQPhGxwDZ8jJ9bbRH-SUUmPYS8RXqssm8v5vKtQi8DrR6bhihftoIl4ZQsKQZ4XKE&usqp=CAU'
+    ),
+  ];
   constructor() {}
   ngOnInit(): void {}
 }
