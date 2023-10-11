6189002d8898631657e2e023057ba3c79e060246
p.hadavi <peymanhc@gmail.com>
2023-10-11 13:11:00 +0330

    :up: (shopping list): improve shopping list with services

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredients.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppingListService],
 })
-export class ShoppingListComponent {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 15),
-  ];
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[];
+  constructor(private slService: ShoppingListService) {}
+  ngOnInit(): void {
+    this.ingredients = this.slService.getIngredients();
+  }
   onIngredientAdded(ingredient: Ingredient): void {
     this.ingredients.push(ingredient);
+    this.slService.ingredientsChanged.subscribe((ingredients:Ingredient[])=>{
+      this.ingredients = ingredients
+    })
   }
 }
