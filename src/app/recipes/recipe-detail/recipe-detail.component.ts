e8c2cfaad44e9e554741f4fdb6e1f723b835cf6c
p.hadavi <peymanhc@gmail.com>
2023-10-11 15:22:37 +0330

    :up: (ingredients): add ingredients to slService

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe-list/recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,6 +8,10 @@ import { Recipe } from '../recipe-list/recipe.model';
   styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
+  constructor(private recipeService: RecipeService) {}
   @Input() recipe: any;
   ngOnInit(): void {}
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe-list/recipe.model';
 import { Ingredient } from '../shared/ingredients.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
@@ -9,22 +11,21 @@ export class RecipeService {
       'Peymanhc',
       'lorem ipsum dolor sit amet, consectetur adip',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_LDgQPhGxwDZ8jJ9bbRH-SUUmPYS8RXqssm8v5vKtQi8DrR6bhihftoIl4ZQsKQZ4XKE&usqp=CAU',
-      [
-         new Ingredient('Meat', 1),
-         new Ingredient('French Fries', 10),
-    ]
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 10)]
     ),
     new Recipe(
       'MaryamZaki',
       'A description of the recipe',
       'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
-      [
-        new Ingredient('Test', 11),
-        new Ingredient('Meat', 10),
-   ]
+      [new Ingredient('Test', 11), new Ingredient('Meat', 10)]
     ),
   ];
+
+  constructor(private slService: ShoppingListService) {}
   getRecipe() {
     return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
