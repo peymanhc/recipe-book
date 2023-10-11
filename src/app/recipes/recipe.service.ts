953f2071b8dc32e40068cbc1cd5f02f5659fd5f5
p.hadavi <peymanhc@gmail.com>
2023-10-11 15:08:49 +0330

    :up: (ingredients): add ingredients model and styles

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe-list/recipe.model';
+import { Ingredient } from '../shared/ingredients.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -7,12 +8,20 @@ export class RecipeService {
     new Recipe(
       'Peymanhc',
       'lorem ipsum dolor sit amet, consectetur adip',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_LDgQPhGxwDZ8jJ9bbRH-SUUmPYS8RXqssm8v5vKtQi8DrR6bhihftoIl4ZQsKQZ4XKE&usqp=CAU'
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_LDgQPhGxwDZ8jJ9bbRH-SUUmPYS8RXqssm8v5vKtQi8DrR6bhihftoIl4ZQsKQZ4XKE&usqp=CAU',
+      [
+         new Ingredient('Meat', 1),
+         new Ingredient('French Fries', 10),
+    ]
     ),
     new Recipe(
       'MaryamZaki',
       'A description of the recipe',
-      'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg'
+      'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+      [
+        new Ingredient('Test', 11),
+        new Ingredient('Meat', 10),
+   ]
     ),
   ];
   getRecipe() {
